979cc69c950295ff2ab1cc277ba977784750b578
ismael <fmartin@gmail.com>
2021-04-13 16:15:06 +0200

    Initial commit

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+ * Project name: DAM20/PACKAGE_NAME
+ * Filename:
+ * Created:  14/11/2020 / 13:47
+ * Description:
+ * Revision:
+ *
+ * @Author: Ismael - fmartin@nigul.cide.es
+ * @Version:
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+ }
+
---
+++ src/main/java/com/example/servingwebcontent/NewController.java
@@ -0,0 +1,27 @@
+package com.example.servingwebcontent;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Project name: DAM20/PACKAGE_NAME
+ * Filename:
+ * Created:  14/11/2020 / 13:47
+ * Description:
+ * Revision:
+ *
+ * @Author: Ismael - fmartin@nigul.cide.es
+ * @Version:
+ */
+@Controller
+public class NewController {
+    @PostMapping("/form")
+    public String form(String personName,String personAddress){
+//        return String.format("simple response. name: %s, address: %s",
+//                personName, personAddress);
+    return "form";
+    }
+}
